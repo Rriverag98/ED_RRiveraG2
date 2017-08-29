769c6a9e04722d4f2956d1f270eef8fcbd5f17fb
Rriverag98 <rriverag98@gmail.com>
2017-08-28 20:13:28 -0500

    Update ED_RRiveraG2_A03.java

--- ED_RRiveraG2_A03/src/ed_rriverag2_a03/ED_RRiveraG2_A03.java
+++ ED_RRiveraG2_A03/src/ed_rriverag2_a03/ED_RRiveraG2_A03.java
@@ -16,10 +16,31 @@ public class ED_RRiveraG2_A03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Secuencial a = new Secuencial();
+        /*Secuencial a = new Secuencial();
         a.setV(a.randomV(10));
+        System.out.println("-----------");
         a.showV();
+        System.out.println("-----------");
         a.showF(a.Fetch(30));
+        System.out.println("-----------");
+        Binario b = new Binario ();
+        b.setV(a.randomV(10));
+        b.showV();
+        System.out.println("-----------");
+        b.insertion();
+        b.showV();
+        System.out.println("-----------");
+        b.showF(b.Fetch(15));*/
+        Binario c = new Binario ();
+        c.setV(c.randomV(10));
+        c.showV();
+        System.out.println("-----------");
+        int ultimo = (c.vector.length-1);
+        int primero = 0;
+        c.mergeSort(primero,ultimo);
+        c.showV();
+        System.out.println("-----------");
+        c.showF(c.Fetch(15));
     }
     
 }
