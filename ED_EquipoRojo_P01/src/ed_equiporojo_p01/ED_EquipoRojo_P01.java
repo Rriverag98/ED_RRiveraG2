e8fed5395ea96925961f2636bde88b98071b0947
Rriverag98 <rriverag98@gmail.com>
2017-09-10 13:33:45 -0500

    Update ED_EquipoRojo_P01.java

--- ED_EquipoRojo_P01/src/ed_equiporojo_p01/ED_EquipoRojo_P01.java
+++ ED_EquipoRojo_P01/src/ed_equiporojo_p01/ED_EquipoRojo_P01.java
@@ -13,85 +13,32 @@ import java.util.*;
 public class ED_EquipoRojo_P01 {
     
     public static Scanner scanner = new Scanner(System.in);
-    public static Cinemex sala1 = new Cinemex();
-    public static Cinemex sala2 = new Cinemex();
-    public static Cinemex sala3 = new Cinemex();
-    public static Cinemex sala4 = new Cinemex();
-    public static Cinemex sala5 = new Cinemex();
-    public static Cinemex sala6 = new Cinemex();
-    public static Cinemex sala7 = new Cinemex();
+    public static ArrayList <Sala> Cinemex = new ArrayList();
+    public static ArrayList <String> nombres = new ArrayList(Arrays.asList("Avatar","Titanic","Star Wars Ep. VII: The Force Awakens",
+    "Jurassic World","The Avengers","Furious 7","Avengers: Age of Ultron","Harry Potter and the Deathly Hallows: Part II","Frozen"
+    ,"Beauty and the Beast","The Fate of the Furious","IronMan 3","Minions","Captain America: Civil War","The Lord of the Rings: The Return of the King"));
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        for (int i=0;i<15;i++){
-        System.out.println("Ingrese el nombre de la pelicula numero "+(i+1)+":");
-        addPelicula();
+        peliculas();
+        for(int j = 0; j<7; j++) {
+        System.out.println("La pelicula mas vista en la sala "+(j+1)+" fue:");
+        System.out.println(Cinemex.get(j).mostViewed());   
         }
-        listPeliculas();
-        System.out.println("------------");
-        System.out.println("Sala 1");
-        System.out.println(sala1.mostViewed());
-        System.out.println("Sala 2");
-        System.out.println(sala2.mostViewed());
-        System.out.println("Sala 3");
-        System.out.println(sala3.mostViewed());
-        System.out.println("Sala 4");
-        System.out.println(sala4.mostViewed());
-        System.out.println("Sala 5");
-        System.out.println(sala5.mostViewed());
-        System.out.println("Sala 6");
-        System.out.println(sala6.mostViewed());
-        System.out.println("Sala 7");
-        System.out.println(sala7.mostViewed());
+        
 
 }
-    public static void addPelicula(){
-        Scanner read = new Scanner (System.in);
-        String nombre;
-        int asistentes;
-        nombre = read.nextLine();
-        asistentes = (int) Math.ceil(Math.random() * 100);
-        
-        if(sala1.addPelicula(nombre,asistentes)){
-            asistentes = (int) Math.ceil(Math.random() * 100);
-            sala2.addPelicula(nombre,asistentes);
-            asistentes = (int) Math.ceil(Math.random() * 100);
-            sala3.addPelicula(nombre,asistentes);
-            asistentes = (int) Math.ceil(Math.random() * 100);
-            sala4.addPelicula(nombre,asistentes);
-            asistentes = (int) Math.ceil(Math.random() * 100);
-            sala5.addPelicula(nombre,asistentes);
-            asistentes = (int) Math.ceil(Math.random() * 100);
-            sala6.addPelicula(nombre,asistentes);
-            asistentes = (int) Math.ceil(Math.random() * 100);
-            sala7.addPelicula(nombre,asistentes);
-                    
-            System.out.println("Pelicula añadida");
-        }else{ 
-        System.out.println("Esa pelicula ya esta en la lista");
-        }
-    }
     
-    public static void listPeliculas(){
-        System.out.println("Asistentes a cada pelicula en cada sala:");
-        System.out.println("Sala 1:");
-        sala1.listPeliculas();
-        System.out.println("Sala 2:");
-        sala2.listPeliculas();
-        System.out.println("Sala 3:");
-        sala3.listPeliculas();
-        System.out.println("Sala 4:");
-        sala4.listPeliculas();
-        System.out.println("Sala 5:");
-        sala5.listPeliculas();
-        System.out.println("Sala 6:");
-        sala6.listPeliculas();
-        System.out.println("Sala 7:");
-        sala7.listPeliculas();
-        
+    public static void peliculas(){
+         for(int j = 0; j<7; j++) Cinemex.add(new Sala(j+1));
+         for(int i = 0; i<15; i++){
+            for(int j = 0; j<7; j++) {
+                Cinemex.get(j).addPelicula(nombres.get(i));
+                int p = Cinemex.get(j).Fetch(nombres.get(i));
+                Cinemex.get(j).peliculas.get(p).setAsistentes((int) Math.ceil(Math.random() * 100));
+            }    
+         }
     }
-    
 }
-
