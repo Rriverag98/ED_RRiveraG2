65fd02f273f5e503f619bd42e3bf70e92c28ad48
Rriverag98 <rriverag98@gmail.com>
2017-09-12 07:44:10 -0500

    Update and rename Nodo.java to Node.java

--- ED_RRiveraG2_A06/src/ed_rriverag2_a06/Node.java
+++ ED_RRiveraG2_A06/src/ed_rriverag2_a06/Node.java
@@ -9,7 +9,15 @@ package ed_rriverag2_a06;
  *
  * @author Rodrigo
  */
-public class Nodo {
-    int dato;
-    Nodo siguiente;
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next=null;
+    }
+    
+    
+    
 }
